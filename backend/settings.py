8e48dce2d1f172535a247b4ac2fc14cac6278311
Nic Scobey <nicscobey@gmail.com>
2022-01-14 13:11:21 -0800

    update cors

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b)eq7v2x)s(!lf$oo(aff(w7_)tz5jd8+-=06bwag*+j!o^*1&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
