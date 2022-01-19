f038f60388f5ddf6a8f135fcef4b8efd8977d98f
Nic Scobey <nicscobey@gmail.com>
2022-01-18 23:00:21 -0800

    added author to blog serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,6 @@
 from api.models import Blog
 from django.contrib.auth.models import User, Group
-from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
-
-
-class BlogSerializer(ModelSerializer):
-    class Meta:
-        model = Blog
-        fields = ["id", "title", "subtitle", "content",
-                  "theme", "created_at", "updated_at"]
+from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer, SerializerMethodField
 
 
 class UserSerializer(ModelSerializer):
@@ -15,6 +8,29 @@ class UserSerializer(ModelSerializer):
         model = User
         fields = ["id", "username", "password", "email",
                   "first_name", "last_name", "groups"]
+
+
+class BlogSerializer(ModelSerializer):
+
+    # author = SerializerMethodField('get_id_from_author')
+    # author = UserSerializer()
+
+    class Meta:
+        model = Blog
+        fields = ["id", "title", "subtitle", "content",
+                  "theme", "created_at", "updated_at", "author"]
+
+    # def get_id_from_author(self, blog):
+    #     author = blog.author.id
+    #     print(author)
+    #     print('🌴🌴🌴')
+    #     return author
+
+    # def create(self, request):
+    #     if request.method == 'POST':
+    #         print('🌴🌴🌴')
+    #         print(self)
+    #         print(request)
 
 
 class GroupSerializer(HyperlinkedModelSerializer):
